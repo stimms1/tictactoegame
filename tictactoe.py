a4ab399467d8d6676de128309e2a57adf0321ac6
stimms1 <bluearinsol0718@gmail.com>
2025-09-24 12:17:16 +0800

    first commit

---
+++ tictactoe.py
@@ -0,0 +1,78 @@
+def printMatrix():
+    print(matrix[0])
+    print(matrix[1])
+    print(matrix[2])
+
+def playTurn(symbol, oppositeSymbol):
+    loop = True
+    while loop == True:
+        print(f"{symbol}'s turn!")
+        xIntercept = int(input("What coordinate in the X axis? "))-1
+        yIntercept = int(input("What coordinate in the Y axis? "))-1
+        if matrix[xIntercept][yIntercept] == symbol or matrix[xIntercept][yIntercept] == oppositeSymbol:
+            print("Invalid input!")
+        else: 
+            matrix[xIntercept][yIntercept] = symbol
+            printMatrix()
+            loop = False
+    
+def checkWinner(matrix, symbol):
+    if (matrix[0][0] == symbol and matrix[0][1] == symbol and matrix[0][2] == symbol) or (matrix[1][0] == symbol and matrix[1][1] == symbol and matrix[1][2] == symbol) or (matrix[2][0] == symbol and matrix[2][1] == symbol and matrix[2][2] == symbol) or (matrix[0][0] == symbol and matrix[1][0] == symbol and matrix[2][0] == symbol) or (matrix[0][1] == symbol and matrix[1][1] == symbol and matrix[2][1] == symbol) or (matrix[0][2] == symbol and matrix[1][2] == symbol and matrix[2][2] == symbol) or (matrix[0][0] == symbol and matrix[1][1] == symbol and matrix[2][2] == symbol) or (matrix[0][2] == symbol and matrix[1][1] == symbol and matrix[2][0] == symbol):
+        return True
+    else:
+        return False
+    
+def checkDraw(matrix):
+    for row in matrix: 
+        for cell in row:
+            if cell == '_':
+                return False
+    return True
+
+def randomizer():
+    return random.randint(0,1)
+import random
+
+matrix = [
+    ['_', '_', '_'],
+    ['_', '_', '_'],
+    ['_', '_', '_']
+]
+randomizer = randomizer()
+isThereWinner = False
+
+printMatrix()
+
+symbol = 'X'
+opposite = 'O'
+if randomizer == 1:
+    print("O goes first!")
+    symbol = 'O'
+    opposite = 'X'
+else: 
+    print("X goes first!")
+
+while checkWinner(matrix, symbol) == False and checkWinner(matrix, opposite) == False:
+    playTurn(symbol, opposite)
+    symbol, opposite = opposite, symbol
+    if checkWinner(matrix, symbol) == True:
+        print(f"{symbol} wins!")
+        break
+    elif checkWinner(matrix, opposite) == True:
+        print(f"{opposite} wins!")
+    else:
+        if checkDraw(matrix):
+            print("It's a tie!")
+            break
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tictactoegui.py
@@ -0,0 +1,50 @@
+#import tictactoe
+import tkinter as tk
+
+
+root = tk.Tk()
+root.title("Tic Tac Toe") 
+root.geometry("800x500")
+root.configure(bg="light gray")
+
+label = tk.Label(root, text="Tic Tac Toe", font=("Arial", 24), bg="light gray")
+label.pack(pady=15, padx=20)
+
+button1 = tk.Button(root, text="Start Game", font=("Arial", 18), width=10, height=2)
+button1.pack()
+button1.place(x=325, y=400)
+
+# row 1 
+button2 = tk.Button(root, text="", font=("Arial", 18), width=5, height=2) #command=root.quit
+button2.pack()
+button2.place(x=250, y=100)
+
+button3 = tk.Button(root, text="", font=("Arial", 18), width=5, height=2) #command=root.quit
+button3.pack()
+button3.place(x=345, y=100)
+
+button4 = tk.Button(root, text="", font=("Arial", 18), width=5, height=2) #command=root.quit
+button4.pack()
+button4.place(x=440, y=100)
+
+#row 2
+button5 = tk.Button(root, text="", font=("Arial", 18), width=5, height=2) #command=root.quit
+button5.pack()
+button5.place(x=250, y=195)
+
+button6 = tk.Button(root, text="", font=("Arial", 18), width=5, height=2) #command=root.quit
+button6.pack()
+button6.place(x=345, y=195)
+
+button7 = tk.Button(root, text="", font=("Arial", 18), width=5, height=2) #command=root.quit
+button7.pack()
+button7.place(x=440, y=195)
+
+#row 3 
+
+
+root.mainloop()
+
+
+if __name__ == "__main__":  
+    root.mainloop()
